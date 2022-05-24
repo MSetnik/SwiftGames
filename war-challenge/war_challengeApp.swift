b9f4339115e4b031cfd81fc0e98901e235b08d38
Matko Setnik <setnik99@gmail.com>
2022-05-24 09:55:34 +0200

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Matko Setnik on 24.05.2022..
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
